9d93d9869942773e9e3280dabb6b8e3b4c00f9c1
Deividas555 <145341758+Deividas555@users.noreply.github.com>
2024-03-20 17:45:03 +0200

    Add files via upload

---
+++ projektas555/Deividas550/js/rawJs/dropdown.js
@@ -0,0 +1,11 @@
+function dropdownas(id){
+    this.open = !this.open
+    dokas = document.getElementById(id)
+    this.open ? dokas.style.display = "none" : dokas.style.display = "block"
+}
+function ez(id){
+    document.getElementById(id).style.display = 'block';
+}
+function ez5(id){
+    document.getElementById(id).style.display = 'none';
+}
